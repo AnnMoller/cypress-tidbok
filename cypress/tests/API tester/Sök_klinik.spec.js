68eb625fd7c070387d12967455bae0aa83e06df3
PanseAB <ann@fredamo.se>
2021-04-13 19:28:49 +0200

    added API test

--- cypress/tests/API tester/Sök_klinik.spec.js
+++ cypress/tests/API tester/Sök_klinik.spec.js
@@ -1,18 +1,29 @@
 
 describe('API Clinics', () => {
-    beforeEach(() => {
-  // alias the users fixtures
-  cy.fixture('users.json').as('users')
-})
-    const user = this.users[0]
+   
+    //const user = this.users[0]
     /*cy.get('header').should('contain', user.name)*/
 
+  /*
     it('GET Clinics', () => {
-      cy.visit(cy.env(api_server))
-      cy.request('/Clinics').as('clinics')
-      cy.get('@clinics').should(response.code('200'))
-
+      // cy.visit(cy.env(api_server))
+        cy.request('/Clinics').as('clinics')
+        cy.get('@clinics').should((response => {
+          expect(response.status).to.eq(200),
+          expect(response).to.have.property('headers')
+        })
     })
 
-  })
+    */
+
+    it('cy.request() - make an XHR request', () => {
+      // https://on.cypress.io/request
+      cy.request('/Clinics')
+       .should((response) => {
+        expect(response.status).to.eq(200)
+
+       })
+     })
+   
+})
 
